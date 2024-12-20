2403bbb015e6e52c26c23b3eb0b7dba9c9c3a425
shizuketsu <evdokimovnikita501@gmail.com>
2024-12-20 13:53:29 +0300

    added strcat

--- src/main.c
+++ src/main.c
@@ -4,7 +4,8 @@
 int main(void)
 {
     printf("Include str.h\n");
-    str_t* str = createStr(20, "Hello, world");
-    printf("%s\n", getStr(str));
+    str_t* str = createStr(20, "aboba");
+    str_t* str2 = createStr(20, "aboba");
+    printf("%s\n", catStr(str, str2)->data);
     return 0;
 }
--- src/str.h
+++ src/str.h
@@ -2,17 +2,21 @@
 #define STR
 
 #include <stdlib.h>
+#include <wchar.h>
+#include <stdio.h> // TODO: delete in production
 
 typedef struct str {
     char* data;
     int length;
 } str_t;
 
-str_t* setStr(str_t* str, char* data);
+str_t* setStr(str_t* str, const char* data);
 void freeStr(str_t* str);
 char* getStr(str_t* str);
+int getStrLength(str_t* str);
+str_t* catStr(str_t* right, str_t* left);
 
-str_t* createStr(int length, char* data)
+str_t* createStr(int length, const char* data)
 {
     str_t* string = (str_t*)malloc(sizeof(str_t));
     if(string == NULL)
@@ -41,7 +45,7 @@ void freeStr(str_t* str)
     free(str);
 }
 
-str_t* setStr(str_t* str, char* data) // она, наверное, не должна возвращать строку
+str_t* setStr(str_t* str, const char* data)
 {
     if(str == NULL || data == NULL) return NULL;
     
@@ -80,6 +84,35 @@ int getStrLength(str_t* str)
     }
 
     return i;
+}
+
+str_t* catStr(str_t* right, str_t* left)
+{
+    if(right == NULL || left == NULL || right->data == NULL || left->data == NULL) return NULL;
+
+    int right_length = getStrLength(right);
+    int left_length = getStrLength(left);
+    int total_length = right_length + left_length;
+
+    str_t* result = createStr(total_length, "");
+    if(result == NULL) return NULL;
+
+    int i = 0;
+
+    for(int j = 0; j < right_length; j++) 
+    {
+        result->data[i] = right->data[j];
+        i++;
+    }
+
+    for(int j = 0; j < left_length; j++) 
+    {
+        result->data[i] = left->data[j];
+        i++;
+    }
+
+    result->data[result->length] = '\0';
+    return result;
 }
 
 #endif
